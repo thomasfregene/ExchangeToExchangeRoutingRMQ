eda1e5b75326f69e3ebe56381ce188df8dde2ec5
thomasfregene <fregzythomas@gmail.com>
2023-12-16 21:13:08 +0100

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Consumer
+
+var factory = new ConnectionFactory{ HostName = "localhost"};
+using var connection = factory.CreateConnection();  
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "secondexchange", type: ExchangeType.Fanout);
+
+channel.QueueDeclare(queue: "letterbox1");
+channel.QueueBind("letterbox1", "secondexchange", "");
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (sender, args) =>
+{
+    var body = args.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Received new message: {message}");
+};
+
+channel.BasicConsume(queue: "letterbox1", autoAck: true, consumer: consumer);
+
+Console.WriteLine("Consuming");
+Console.ReadKey();
---
+++ Producer/Program.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//Producer
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "firstexchange", type: ExchangeType.Direct);
+channel.ExchangeDeclare(exchange: "secondexchange", type: ExchangeType.Fanout);
+
+channel.ExchangeBind("secondexchange", "firstexchange", "");
+
+var message = "This message has gone through multiple exchange";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish("firstexchange", "", null, body);
+
+Console.WriteLine($"Sending message: {message}");
